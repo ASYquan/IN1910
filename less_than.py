9b79299b6731c24bc4b7848f6b5182458d69a84c
Andrew Sheng Ye Quan <77693509+ASYquan@users.noreply.github.com>
2021-08-24 19:28:16 +0200

    Gjorde det oppgaven faktisk ba om, istedenfor å teste

--- less_than.py
+++ less_than.py
@@ -1,1 +1,12 @@
-print("Less than")
+
+def less_than(original,n):
+    lst = [i for i in original if i<n ]
+    return lst
+
+original = [1,2,3,4,5]
+
+for n in range(5):
+    if len(less_than(original,n)) == 0:
+        print("tom")
+    else:
+        print(f"Liste av tall som er mindre enn {n}: {less_than(original, n)}")
